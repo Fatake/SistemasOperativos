adbf00991061d251ed775c4f6e6e2892f4345ec3
Fatake Raicos <38116322+Fatake@users.noreply.github.com>
2019-08-12 11:30:20 -0500

    Procesos While y Variable

---
+++ Practica 1/ProcesosVariableIndepe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+//Main
+int main(int argc, char **argv){
+	int pid,b=-1,*a;
+	pid = fork();
+	a=&b;
+	while(1)
+		if(pid == 0)
+			while(1);
+				printf("Proceso Hijo %i",(*a)++);
+		else
+			while(1);
+				printf("Proceso Padre %i",--(*a));
+
+	return 0;
+}
