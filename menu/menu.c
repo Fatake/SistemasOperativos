f2ad2edf5e62a078e13d853640d0e5e1a452e8dd
Fatake Raicos <pace_lozano@hotmail.com>
2019-10-05 10:28:58 -0500

    Signal Add

--- menu/menu.c
+++ menu/menu.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <signal.h>
+#include <stdlib.h>
 #include "semaforos.h"
 #define limpia() printf("\033[H\033[J")
 #define True 1
@@ -12,14 +14,16 @@
 //
 char *gets(char *s);
 int menuPrincipal();
-int  menuMemoria(int shmid,int valor);
+int menuMemoria(int shmid,int valor);
+int menusemaforo(int semid, int valor);
+void lanzaSenal();
 void wait();
 //
 //Main
 //
 int main(int argc, char **argv){
-	int m = 1, mo, shmid = 0,aux = 0;
-	int *entero ;
+	int m = 1, mo, shmid = 0,aux = 0, semid = 0;
+	int *entero;
 	//---------------------------------------------------------------------------------
 	do{
 		m = menuPrincipal();
@@ -29,15 +33,18 @@ int main(int argc, char **argv){
 					mo = menuMemoria(shmid, aux);
 					switch(mo){
 						case 1://Crea Memoria Compartida
-							shmid = shmget(0,sizeof(int),IPC_PRIVATE|IPC_CREAT|0666);
-							entero = (int *) shmat(shmid,0,0);
-							aux = *entero = 0;
-							printf("Memoria Creada \n shmid: %d valor: %d",shmid,*entero);
+							if(shmid == 0){
+								shmid = shmget(0,sizeof(int),IPC_PRIVATE|IPC_CREAT|0666);
+								entero = (int *) shmat(shmid,0,0);
+								aux = *entero = 0;
+								printf("Memoria Creada \n shmid: %d valor: %d",shmid,*entero);
+							}else
+								printf("Usted ya tiene una memoria creada u.u\n");
 							wait();
 						break;
 						case 2://Modifica Memoria
 							if(shmid != 0){
-								printf("\nshmid: %d valor: %d",shmid,*entero);
+								printf("\nShmid: %d valor: %d\n",shmid,*entero);
 								printf("Ingrese un nuevo valor\n->");
 								scanf("%d",entero);
 								aux = *entero;
@@ -45,26 +52,66 @@ int main(int argc, char **argv){
 								printf("No a creado una memoria u.u");
 							wait();
 						break;
-						case 3:
+						case 3://Borrar Memoria
 							if(shmid != 0){
-								printf("\nshmid: %d valor: %d",shmid,*entero);
-								shmctl(shmid,IPC_RMID,0);
+								printf("\nshmid: %d valor: %d\n",shmid,*entero);
+								shmctl(shmid,IPC_RMID,NULL);
+								shmid = 0;
+								entero = NULL;
 								aux = 0;
+								printf("Memoria Borrada n.n\n");
 							}else
 								printf("No a creado una memoria u.u");
 							wait();
-
 						break;
 					}
 				}while( mo != 0);
-				break;
-
-			case 2:
-				printf("Semaforos");
-				break;
+			break;
+			case 2://Semaforo
+				 do{
+					mo = menusemaforo(semid, aux);
+					switch(mo){
+						case 1://Crear Semaforo
+							if(semid == 0){
+								semid = creaSemaforo(0,0);
+								if(semid < 0){
+									printf("Error al crear un semaforo u.u\n");
+									semid = 0;
+								}else{
+									aux = 0;
+									printf("\n Semid : %d, Valor: %d",semid,aux);
+								}
+							}else
+								printf("Ya tiene creado un semaforo u.u\n");
+							wait();
+						break;
+						case 2://Modificar Valor Semaforo
+							if(semid != 0){
+								printf("Por hacer");
+							}else
+								printf("No a creado ningun semaforo u.u\n");
+							wait();
+						break;
+						case 3://Borrar Semaforo
+							if(semid != 0){
+								if(destruyeSemaforo(semid,0) > 0){
+									aux = 0;
+									semid = 0;
+									printf("Semaforo Destruido n.n\n");
+								}else
+									printf("No se pudo destruir el semaforo");
+							}else
+								printf("No a creado ningun semaforo u.u\n");
+							wait();
+						break;
+					}
+				}while( mo != 0);
+			break;
 			case 3:
 				printf("Sellañes entre procesos");
-				break;
+				signal(SIGINT,lanzaSenal);
+				while(True){};
+			break;
 			default:
 				if(m != 0)
 					printf("\n Opcion no disponible \n");
@@ -115,7 +162,7 @@ int menuMemoria(int shmid,int valor){
 	printf("\n  Seleccione una opción:\n");
 	if( shmid != 0)
 		printf("\n  Shmid: %d, Valor: %d",shmid,valor);
-	
+
 	printf("\n<---------------------->\n\n");
 	printf("1)Crear memoria\n");
 	printf("2)Modificar valor\n");
@@ -126,4 +173,30 @@ int menuMemoria(int shmid,int valor){
 	scanf("%d",&m);
     limpia();
 	return m;
+}
+/*
+ * Menu Semaforo
+ */
+ int menusemaforo(int semid, int valor){
+	int m;
+	limpia();
+	printf("\n			Menu Semaforo\n");
+	printf("\n  Seleccione una opción:\n");
+	if( semid != 0)
+		printf("\n Semid : %d, Valor: %d",semid,valor);
+
+	printf("\n<---------------------->\n\n");
+	printf("1)Crear Semaforo\n");
+	printf("2)Modificar valor\n");
+	printf("3)Borrar Semaforo\n");
+	printf("\n<---------------------->\n\n");
+	printf("\n0)Menu Principal.\n");
+	printf("\n-> ");
+	scanf("%d",&m);
+    limpia();
+	return m;
+}
+//
+void lanzaSenal(){
+	printf("Señal cachada \n");
 }
