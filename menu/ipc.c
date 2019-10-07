a606fa087c166b7aa19699d68a27e62addb081a6
Fatake Raicos <pace_lozano@hotmail.com>
2019-10-07 17:20:59 -0500

    Avance IPC

--- menu/ipc.c
+++ menu/ipc.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-
+/*
+ * Main
+ */
 int main(int argc, char **argv){
-	if(argc >= 2)
-		if(!strcmp(argv[1],"-m"))
-			if(!strcmp(argv[2],"-c")){
+	if(argc >= 4){
+		if(!strcmp(argv[1],"-m"))//Memoria
+			if(!strcmp(argv[2],"-c"))//Crear
 				printf("crear memoria\n");
-			}else
-				printf("Zona de Memoria Compartida");
-		else if(!strcmp(argv[1],"-s")){
+			else if(!strcmp(argv[2],"-d"))//Borrar
+				printf("Borrar Memoria\n");
+
+		else if(!strcmp(argv[1],"-s")){//Semaforo
 			printf("Zona de Semaforo");
-		}else if(!strcmp(argv[1],"-w")){
+
+		}else if(!strcmp(argv[1],"-w")){//Mensajes
 			printf("Zona de Mensajes");
-		}else if(!strcmp(argv[1],"--help"))
-			printf("Parametros admitidos:\n -m Memoria Comparitda \n -s Semaforo \n -w Mensajes\n");
-		else
-			printf("Comando no encontrado\n Utilice el parametro --help ");
-	else
-		printf("Comando no encontrado\n Utilice el parametro --help ");
+
+		}else
+			printf("Parametro no encontrado\nUtilice --help Para obtener ayuda");
+	}else{
+		if(argc < 4){
+			if(argc > 1 && !strcmp(argv[1],"--help")){
+				printf("Programa que crear recursos IPC \n");
+				printf("Parametros admitidos:\n");
+				printf("-m <Memoria Comparitda> -c <Crear> <Int:Tamanio>\n");
+				printf("-m <Memoria Comparitda> -d <Borrar> <Int:shmid>\n");
+				printf("-s <Semaforo> -c <Crear> <Int:Cantidad> <Int:Valor Inicial>\n");
+				printf("-s <Semaforo> -d <Borrar> <Int:semid>\n");
+				printf("-w <Mensajes> -c <Crear> <Parametros>\n");
+				printf("-w <Mensajes> -d <Borrar> <mensajeID>\n");
+				printf("Ejemplo:\n");
+				printf("./ipc -s -c 3 0\n");
+				printf("Creara 3 semaforos con valor 0\n");
+			}else
+				printf("Parametro no encontrado\nUtilice --help Para obtener ayuda");
+		}
+	}
 	printf("\n\n");
 	return 0;
 }
