940279c9a5c44044bf044d0587174e5a58702b82
Fatake Raicos <pace_lozano@hotmail.com>
2019-10-14 11:36:04 -0500

    Filosofos ADD

---
+++ Problema de los 5 Philosofos/Filosofo.c
@@ -0,0 +1,48 @@
+#include "ipcfunctions.h"
+#include <stdio.h>
+
+//Macros y Definiciones
+#define N 5
+#define left (N+i-1)%N
+#define right (N+i+1)%N
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+
+//Tipos
+typedef int semaforo;
+
+int estado[N];
+semaforo mutex;
+semaforo sem[N];
+int i;
+
+void filosofo(int numeroFilosofo);
+void takeForks(int i);
+void putForks(int i);
+void comprueba(int i);
+
+/*
+ * Main
+ */
+int main(int argc, char **argv){
+	printf("give me a bottle of rum!\n");
+	//inicializar el estado en pensando
+	for(i = 0; i < N ; i++)
+		estado[i] = 0;
+	//Semaforo de mutex
+	mutex = creaSemaforo(0,1);
+	//Semaforos para cada Filosofo
+	for(i = 0; i < N ; i++)
+		sem[i] = creaSemaforo(0,0);
+	
+	printf("\nSemaforo mutex : %d\n",mutex);
+	for ( i = 0; i < N; i++)
+		printf("Semaforo numero %d : %d\n",(i+1),sem[i]);
+	
+	for ( i = 0; i < N; i++)
+		destruyeSemaforo(sem[i],0);
+	destruyeSemaforo(mutex,0);
+	
+	return 0;
+}
